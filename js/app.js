dc0b634f344824e38ddee3679a6e234d57bdfc5c
Tonyzetoile <tonyzetoile@gmail.com>
2026-08-31 13:58:44 +0200

    Not important change but change anyway

--- js/app.js
+++ js/app.js
@@ -1,2 +1,3 @@
-addEventListener("DOMContentLoaded", event =>
-    window.alert("Portfolio en Work In Progress - Pour l'instant, il n'est pas adapté aux écrans de téléphone portable."))
+addEventListener("DOMContentLoaded", event => {
+    window.alert("Portfolio en Work In Progress - Pour l'instant, il n'est pas adapté aux écrans de téléphone portable.")
+})
